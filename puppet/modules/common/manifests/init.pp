47e55706e907016d62f4a79e56e2bc68ce4a9d57
Mathieu Jadin <mathjadin@gmail.com>
2018-05-14 10:20:30 +0200

    Add dependencies

--- puppet/modules/common/manifests/init.pp
+++ puppet/modules/common/manifests/init.pp
@@ -4,5 +4,7 @@ class common {
 	package { 'pkg-config': }
 	package { 'bison': }
 	package { 'flex': }
+	package { 'python': }
+	package { 'libelf-dev': } # Needed for Virtualbox guest additions on kernels above 4.14
 }
 
